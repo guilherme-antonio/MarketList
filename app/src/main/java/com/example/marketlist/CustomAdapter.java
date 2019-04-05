51e2af8367c36078f68de4c04d4e63997396f81b
aluno <guioliveira99@gmail.com>
2019-04-05 20:59:18 -0300

    List Activity

---
+++ app/src/main/java/com/example/marketlist/CustomAdapter.java
@@ -0,0 +1,43 @@
+package com.example.marketlist;
+
+import android.content.Context;
+import android.support.design.widget.FloatingActionButton;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class CustomAdapter extends ArrayAdapter<Item> {
+    private Context _context;
+
+    public CustomAdapter(Context context, int resource, List<Item> objects)
+    {
+        super(context, resource, objects);
+
+        _context = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if(view == null)
+            view = LayoutInflater.from(_context).inflate(R.layout.item,parent,false);
+
+        Item item = getItem(position);
+
+        TextView itemName = view.findViewById(R.id.list_item_name);
+        ImageView itemImage = view.findViewById(R.id.list_item_image);
+        FloatingActionButton itemRemoveButton = view.findViewById(R.id.list_item_remove);
+
+        itemRemoveButton.setTag(position);
+
+        itemName.setText(item.getName());
+        itemImage.setImageDrawable(item.getImage());
+
+        return view;
+    }
+}
